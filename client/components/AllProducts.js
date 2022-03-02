bc58da63366049926aef2be377b692981c67ef93
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-03-02 12:28:29 -0800

    added filtering tags to AllProducts and some styling

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -16,16 +16,29 @@ function AllProducts(props) {
 
   return (
     <main id="all-products">
-      {products.map((product) => {
-        return (
-          <div key={product.id}>
-            <img src={product.imageUrl} />
-            <h2>{product.name}</h2>
-            <h3>{product.price}</h3>
-            <button>Add to Cart</button>
-          </div>
-        );
-      })}
+      <div id="filtering">
+        <h2>Filter</h2>
+        <h3>Region:</h3>
+        <div>Region 1</div>
+        <div>Region 2</div>
+        <div>Region 3</div>
+        <h3>Main Ingredient</h3>
+        <div>Ingredient 1</div>
+        <div>Ingredient 2</div>
+        <div>Ingredient 3</div>
+      </div>
+      <div id="products">
+        {products.map((product) => {
+          return (
+            <div key={product.id}>
+              <img src={product.imageUrl} />
+              <h2>{product.name}</h2>
+              <h3>{product.price}</h3>
+              <button>Add to Cart</button>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
