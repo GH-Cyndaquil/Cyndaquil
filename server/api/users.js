dbd4d48da08a5b64952a526a21f391dfaf5192a5
Matt Yard <mattyard11@gmail.com>
2022-03-03 14:13:59 -0500

    adds put route to update user information

--- server/api/users.js
+++ server/api/users.js
@@ -4,6 +4,17 @@ const {
 } = require("../db");
 module.exports = router;
 
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token);
+    req.userId = user.id;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
 router.get("/", async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -15,5 +26,34 @@ router.get("/", async (req, res, next) => {
     res.json(users);
   } catch (err) {
     next(err);
+  }
+});
+
+router.put("/:id", requireToken, async (req, res, next) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      username,
+      address,
+      city,
+      state,
+      postalCode,
+    } = req.body;
+    const user = await User.findByPk(req.params.id);
+    await user.update({
+      firstName,
+      lastName,
+      email,
+      username,
+      address,
+      city,
+      state,
+      postalCode,
+    });
+    res.send(user);
+  } catch (error) {
+    next(error);
   }
 });
