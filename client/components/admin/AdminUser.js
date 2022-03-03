2840c13bc2e0142a64454f02db8d00a051c8a78f
Peter Cummings <petecummings@hotmail.com>
2022-03-03 13:56:12 -0800

    created basic add admin page

--- client/components/admin/AdminUser.js
+++ client/components/admin/AdminUser.js
@@ -1,50 +1,19 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { getAllUsers, deleteUser } from "../store/user";
+import React from "react";
+import { useSelector } from "react-redux";
 
-export class AllUsers extends Component {
-  constructor() {
-    super();
-    this.deleteUser = this.deleteUser.bind(this);
-  }
+const SingleUser = () => {
+  const user = useSelector((state) => state.user);
 
-  componentDidMount() {
-    this.props.getAllUsers();
-  }
-
-  deleteUser(id) {
-    this.props.deleteUser(id);
-  }
-  render() {
-    return (
+  return (
+    <div className="adminblock">
+      <h1>{user.username}</h1>
       <div>
-        <h1>Admin Page</h1>
-        <h2>All site Users</h2>
-        {this.props.users.map((user) => (
-          <div key={user.id}>
-            <Link to={`/user/${user.id}`}>
-              <div>
-                <h3>{user.name}</h3>
-              </div>
-            </Link>
-            <button type="button" onClick={() => this.deleteUser(user.id)}>
-              Delete this user
-            </button>
-          </div>
-        ))}
+        <h3>Admin Rights</h3>
+        <p>{user.isAdmin}</p>
+        <button>Edit Account</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => ({
-  users: state.user.allUsers,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  getAllUsers: () => dispatch(getAllUsers()),
-  deleteUser: (userId) => dispatch(deleteUser(userId)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(AllUsers);
+export default SingleUser;
