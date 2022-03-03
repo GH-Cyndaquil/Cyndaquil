e2dfa65de3fdb522845a900b106b31bd90e8f78b
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-03-03 09:07:05 -0800

    wrote API route to fetch regions

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ router.use('/users', require('./users'));
 router.use('/products', require('./products'));
 router.use('/orders', require('./orders'));
 router.use('/ingredients', require('./ingredients'));
+router.use('/regions', require('./regions'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
