d76cb29b58e97086680951d44e6ca712bd1585ce
Peter Cummings <petecummings@hotmail.com>
2022-03-03 13:55:22 -0800

    updated links and css

--- client/components/admin/AdminPage.js
+++ client/components/admin/AdminPage.js
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 
 export const AdminPage = () => {
   return (
-    <div>
-      <h1>Admin Page</h1>
-      {/*<Link to="/allUsers">All Users</Link>
-      <br />
-  <Link to="/addproduct">Add Product</Link>*/}
-    </div>
+    <>
+      <div className="adminblock">
+        <h1>Admin Page</h1>
+        <button>
+          <Link to="/adminrights">Admin Rights</Link>
+        </button>
+        <button>
+          <Link to="/addproduct">Add Product</Link>
+        </button>
+      </div>
+    </>
   );
 };
 
