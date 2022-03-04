b38c32ad12955744baadb6a10a132ea564013400
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-03-04 13:11:00 -0800

    styled navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store';
 
-const Navbar = () => {
+const Navbar = (props) => {
   const userId = useSelector((state) => state.user.id);
   const isLoggedIn = useSelector((state) => !!state.user.id);
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
 
   return (
     <div id="navbar">
@@ -14,28 +15,46 @@ const Navbar = () => {
       <nav>
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <Link to="/products">Products</Link>
+          <Link to="/home" className="nav-button">
+            Home
+          </Link>
+          <Link to="/products" className="nav-button">
+            Products
+          </Link>
         </div>
 
         {isLoggedIn ? (
-          <div>
-            <Link to={`/users/${userId}`}>Account</Link>
-            <a href="#" onClick={() => dispatch(logout())}>
+          <div id="nav-user-control">
+            <Link to={`/users/${userId}`} className="nav-button">
+              <div>Hello, {user.username}</div>
+              Account
+            </Link>
+            <a
+              className="nav-button"
+              href="#"
+              onClick={() => dispatch(logout())}
+            >
               Logout
             </a>
+            <Link to="/viewcart" className="nav-button">
+              Cart
+              {/* <h5>{number of items in cart}</h5> */}
+            </Link>
           </div>
         ) : (
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+          <div id="nav-user-control">
+            <Link to="/login" className="nav-button">
+              Login
+            </Link>
+            <Link to="/signup" className="nav-button">
+              Sign Up
+            </Link>
+            <Link to="/viewcart" className="nav-button">
+              Cart
+              {/* <h5>{number of items in cart}</h5> */}
+            </Link>
           </div>
         )}
-
-        <Link to="/viewcart">
-          <img id="cartIcon" src="/images/carticon.png" />
-          {/* <h5>{number of items in cart}</h5> */}
-        </Link>
       </nav>
       <hr />
     </div>
